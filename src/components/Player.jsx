aac783fb175f79ff8e19cfd3a0e269b43fdab6b8
ronakjpatel <rjpatel7991@gmail.com>
2024-06-03 00:10:51 -0400

    added the ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+
+  const playerNameRef = useRef();
+
+  const handleClick = () => {
+    if (playerNameRef.current.value != "") {
+      setPlayerName(playerNameRef.current.value);
+    }
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {!playerName ? "unknown entity" : playerName + " !!!"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerNameRef} />
+        <button onClick={() => handleClick()}>Set Name</button>
       </p>
     </section>
   );
